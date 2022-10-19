4fcade5f7b65a91afb5a063af509798af3c4f548
Erick <emendo42@stu.southtexascollege.edu>
2022-10-19 14:15:52 -0500

    changed message content

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Hello There' })
+  res.send({ msg: "Hello There, This is Erick Mendoza's API" })
 })
 
 app.listen(port)
