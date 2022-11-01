d3b9baf0eac5fdbefeb1a21bf0a008704842dd74
Erick <emendo42@stu.southtexascollege.edu>
2022-11-01 14:58:13 -0500

    added basic auth

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
-
+import basicAuth from 'express-basic-auth'
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
@@ -14,6 +14,11 @@ const port = Number(process.env.PORT)
 
 const app = express()
 
+app.use(
+  basicAuth({
+    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
+  }),
+)
 app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({ origin: process.env.ORIGIN }))
 app.use(helmet())
