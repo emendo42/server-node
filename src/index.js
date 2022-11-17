f0add717efbcdbc696ffbaabb608b990e5c3a368
emendo42 <erickmendoza0960@gmail.com>
2022-11-17 11:25:21 -0600

    fixed src  folder mistakes and typos

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './utils/dotenv'
+import './utils/config'
 
 import express from 'express'
 import morgan from 'morgan'
@@ -6,6 +6,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import bodyParser from 'body-parser'
 import basicAuth from 'express-basic-auth'
+
 import logger from './utils/logger'
 import router from './routes'
 import { notFound, errorHandler } from './utils/errors'
@@ -20,7 +21,7 @@ app.use(
   }),
 )
 app.use(morgan(process.env.MORGAN_LOG))
-app.use(cors({ origin: process.env.ORIGIN }))
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
 app.use(helmet())
 app.use(bodyParser.json())
 
